f467b74a82b427e5e9b12a73138cab1bf7f8f0e2
Yazid9899 <yazidsiddiq9899@gmail.com>
2023-08-08 15:57:38 +0700

    home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,50 @@
-import { View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 const Home = () => {
+  const navigation = useNavigation();
+  const changeScreen = (name) => {
+    navigation.push(name);
+  };
   return (
-    <View>
-      <Text>Home Screen!</Text>
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          changeScreen("Student");
+        }}
+      >
+        <Text style={styles.buttonText}>Student</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          changeScreen("Course");
+        }}
+      >
+        <Text style={styles.buttonText}>Course</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default Home;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#334257",
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
