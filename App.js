94e2d2f5fe45e37d1fb145fbedffb3956fa6e352
Yazid9899 <yazidsiddiq9899@gmail.com>
2023-08-08 15:46:37 +0700

    create navigation route and file

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./screens/Home";
+import Student from "./screens/Student";
+import Course from "./screens/Course";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="home" component={Home} />
+        <Stack.Screen name="Student" component={Student} />
+        <Stack.Screen name="Course" component={Course} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/Student.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Student = () => {
+  return (
+    <View>
+      <Text>Student Screen!</Text>
+    </View>
+  );
+};
+
+export default Student;
